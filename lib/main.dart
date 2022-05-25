9ce7afc3b76dd454d98af5ca675fe9ed0e93e29a
ilen <ilen8vin@gmail.com>
2022-05-25 15:17:11 +0700

    routing to other page

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,15 @@
-// ignore_for_file: deprecated_member_use
-
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'package:myapp/pages/choose_location.dart';
+import 'package:myapp/pages/home.dart';
+import 'package:myapp/pages/loading.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: QuoteList(),
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => const Loading(),
+      '/home': (context) => const Home(),
+      '/location': (context) => const ChooseLocation(),
+    },
   ));
-}
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(
-        text: 'Be yourself; everyone else is already taken',
-        author: 'Oscar Wilde'),
-    Quote(text: 'Whatever the world think, stay true', author: 'Smolbean Ei'),
-    Quote(
-        text: 'You can do anything if you put your mind into it!',
-        author: 'Smolbean Ei'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: const Text(
-          'Ei Quote',
-        ),
-        backgroundColor: Colors.brown[200],
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-      body: Column(
-        children: quotes
-            .map((quote) => QuoteCard(
-                quote: quote,
-                delete: () {
-                  setState(() {
-                    quotes.remove(quote);
-                  });
-                }))
-            .toList(),
-      ),
-    );
-  }
 }
---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text('Choose a location'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: const Text('choose location screen'),
+    );
+  }
+}
