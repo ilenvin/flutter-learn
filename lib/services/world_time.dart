14adf8bc89229b0fd6367e54196708526245ec9c
ilen <ilen8vin@gmail.com>
2022-05-27 15:32:49 +0700

    err handling

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -10,21 +10,27 @@ class WorldTime {
   WorldTime({required this.location, required this.flag, required this.url});
 // Asia/Phnom_Penh
   Future<void> getTime() async {
-    var response =
-        await http.get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-    // var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
-    Map data = convert.jsonDecode(response.body);
+    try {
+      var response = await http
+          .get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      // var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      Map data = convert.jsonDecode(response.body);
 
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
+      // get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
 
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    // ignore: avoid_print
-    print(now);
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      // ignore: avoid_print
+      print(now);
 
-    time = now.toString();
+      time = now.toString();
+    } catch (e) {
+      // ignore: avoid_print
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
   }
 }
